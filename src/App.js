1e2793ccfad2af6ecc755ca6be59cf5cfa87a297
HirokiNakashima <hiroki123naka4ma@gmail.com>
2018-06-27 10:00:39 +0900

    iprops

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-// // クラスコンポーネント
-// class App extends Component {
-//   render() {
-//     return (
-//       <div>Hello!! I'm class component!</div>
-//     )
-//   }
-// }
+// Props 
+// コンポーネントに属性を与える
 
-// 関数コンポーネント
+
 const App = () => {
+  const profiles = [
+    { name: "Taro", age: 10 },
+    { name: "Hanako", age: 7 },
+    { name: "Unknown" }
+  ]
+
   return (
     <div>
-      <h1>Hello!! I'm functional component!</h1>
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index}/>
+        })
+      }     
+
+      {/* <User name={"Taro"} age={20} />
+      <User name={"Hanako"} age={14} /> */}
+
     </div>
     
   )
 }
 
-const Cat = () => {
-  return <div>Meow!!</div>
+const User = (props) => {
+  return <div>Hi, I'm {props.name}! I'm {props.age} years old. </div>
+}
+
+// 指定がなかった場合のデフォルトpropsを定義
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
