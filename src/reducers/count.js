7f0f0aa0d830c5b5e00472e583f19514dfd2b4c3
HirokiNakashima <hiroki123naka4ma@gmail.com>
2018-07-11 09:18:29 +0900

    add ESLint config and refactoring

--- src/reducers/count.js
+++ src/reducers/count.js
@@ -1,13 +1,14 @@
-import { INCREMENT, DECREMENT }  from '../actions'
+import { INCREMENT, DECREMENT } from '../actions'
 
 const initialState = { value: 0 }
+const RATE = 1
 
 export default ( state = initialState, action ) => {
   switch (action.type) {
     case INCREMENT:
-      return { value: state.value + 1 }
+      return { value: state.value + RATE }
     case DECREMENT:
-      return { value: state.value - 1 }
+      return { value: state.value - RATE }
     default:
       return state
   }
